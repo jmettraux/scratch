79b48552000ee265f147e8ae35ab53d641e46072
John Mettraux <jmettraux@gmail.com>
2013-10-17 08:59:31 +0900

    playing with arrays and stack vs heap

--- list/list.c
+++ list/list.c
@@ -25,10 +25,15 @@
 
 #include <time.h>
 #include <stdio.h>
+#include <stdint.h>
 
-#define L 10000000
+#define L (long unsigned)1000000
+  // ~ max on the stack for my GNU/Linux
 
-
+// to use clock_gettime, had to
+//
+//   CFLAGS= -g -Wall -O3 -D_POSIX_C_SOURCE=199309L -lrt
+//
 double now()
 {
   struct timespec ts;
@@ -36,19 +41,46 @@ double now()
   return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
-
 int main(int argc, char *argv[])
 {
   //
   // array
 
-  double start = now();
-  char *array[L];
-  for (int i = 0; i < L; i++)
+//  char *toto = "toto";
+//  char *array[] = { "ab", "cd" };
+//
+//  printf("%s\n", toto);
+//  printf("%s\n", array[0]);
+//  printf("%s\n", array[1]);
+//
+//  char *a2[2];
+//  a2[0] = array[0];
+//  a2[1] = array[1];
+//
+//  printf("%s\n", a2[0]);
+//  printf("%s\n", a2[1]);
+
+  char *toto = "toto";
+  printf("%lu\n", SIZE_MAX);
+  printf("%s\n", toto);
+  //return 0;
+
+  for (int ii = 0; ii < 3; ii++)
   {
-    array[i] = "toto";
+    double start = now();
+
+    char *array[L];
+    for (size_t i = 0; i < L; i++)
+    {
+      //if (i % 10000 == 0) printf("%lu\n", i);
+      array[i] = toto;
+    }
+
+    double delta = now() - start;
+    printf("%fs\n", delta);
   }
-  double delta = now() - start;
-  printf("%fs\n", delta);
+
+  printf("%lu\n", L - 1);
+  //printf("<%s>\n", array[L - 1]);
 }
 
