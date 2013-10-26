6c083e05be2dd7bb3fa319fb18a36c41ac9bef25
John Mettraux <jmettraux@gmail.com>
2013-10-27 06:59:29 +0900

    implement ends_with

--- zest/zest.c
+++ zest/zest.c
@@ -2,11 +2,24 @@
 #include <stdio.h>
 //#include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
 
 // USAGE:
 // zest [dir]
 
 // http://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
+
+int ends_with(char *s, char *end)
+{
+  size_t ls = strlen(s);
+  size_t le = strlen(end);
+
+  if (le > ls) return 0;
+
+  char *ss = s + ls - le;
+
+  return (strncmp(ss, end, le) == 0);
+}
 
 int main(int argc, char *argv[])
 {
@@ -25,8 +38,9 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  while ((ep = readdir(dp)))
+  while ((ep = readdir(dp)) != NULL)
   {
+    if ( ! ends_with(ep->d_name, "_spec.c")) continue;
     printf("file: %s\n", ep->d_name);
   }
   closedir(dp);
