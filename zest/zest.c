fd3ea981998663508b6e42ac35992866f071fbf6
John Mettraux <jmettraux@gmail.com>
2013-10-27 06:47:30 +0900

    let zest list dirs

--- zest/zest.c
+++ zest/zest.c
@@ -1,10 +1,36 @@
 
 #include <stdio.h>
+//#include <sys/types.h>
+#include <dirent.h>
 
 // USAGE:
 // zest [dir]
 
+// http://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
+
 int main(int argc, char *argv[])
 {
+  char *dir = ".";
+  if (argc > 1) dir = argv[1];
+  printf("dir: %s\n", dir);
+
+  DIR *dp;
+  struct dirent *ep;
+
+  dp = opendir(dir);
+
+  if (dp == NULL)
+  {
+    perror("couldn't open directory");
+    return 1;
+  }
+
+  while ((ep = readdir(dp)))
+  {
+    printf("file: %s\n", ep->d_name);
+  }
+  closedir(dp);
+
+  return 0;
 }
 
