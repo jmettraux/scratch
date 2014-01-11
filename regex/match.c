9a8c76ddf7c55df99662949b213efa84293ef117
John Mettraux <jmettraux@gmail.com>
2014-01-12 07:50:51 +0900

    play with regex.h

---
+++ regex/match.c
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <regex.h>
+
+int main()
+{
+  regex_t exp;
+  regcomp(&exp, " [=f!]{3} ", REG_EXTENDED | REG_NOSUB);
+
+  int r;
+  char *s;
+
+  s = "toto === nada";
+  r = regexec(&exp, s, 0, NULL, 0);
+  printf("\"%s\" match ? %s\n", s, (r == 0 ? "yes" : "no"));
+
+  s = "toto ?== nada";
+  r = regexec(&exp, s, 0, NULL, 0);
+  printf("\"%s\" match ? %s\n", s, (r == 0 ? "yes" : "no"));
+}
+
