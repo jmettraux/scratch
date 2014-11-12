0b35aec147c0df3e0d4d787b524b56747e9fe5fe
John Mettraux <jmettraux@gmail.com>
2014-11-12 12:30:16 +0900

    comparing with sha512sum

--- sha/sha.c
+++ sha/sha.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-  char data[] = "lore ipsum";
+  char data[] = "lore ipsum\n";
   char hash[SHA512_DIGEST_LENGTH];
   SHA512((unsigned char *)data, strlen(data), (unsigned char *)hash);
 
@@ -14,7 +14,7 @@ int main()
   for (size_t i = 0; i < SHA512_DIGEST_LENGTH; ++i)
   {
     //printf("i%zu %02x, ", i, (unsigned char)hash[i]);
-    printf("%02x,", (unsigned char)hash[i]);
+    printf("%02x", (unsigned char)hash[i]);
   }
   printf("\n");
 }
