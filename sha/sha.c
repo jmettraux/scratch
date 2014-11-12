156c3e44b30cb0586a4708f2fb31a774909a9893
John Mettraux <jmettraux@gmail.com>
2014-11-12 11:24:41 +0900

    play with sha512

---
+++ sha/sha.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+int main()
+{
+  char data[] = "lore ipsum";
+  char hash[SHA512_DIGEST_LENGTH];
+  SHA512((unsigned char *)data, strlen(data), (unsigned char *)hash);
+
+  printf("%i\n", SHA512_DIGEST_LENGTH);
+  puts(data);
+  for (size_t i = 0; i < SHA512_DIGEST_LENGTH; ++i)
+  {
+    //printf("i%zu %02x, ", i, (unsigned char)hash[i]);
+    printf("%02x,", (unsigned char)hash[i]);
+  }
+  printf("\n");
+}
+
