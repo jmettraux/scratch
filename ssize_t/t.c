93cf92753c6c0529569db5aa9174e0ff87e5af0a
John Mettraux <jmettraux@gmail.com>
2015-08-21 07:18:09 +0900

    cast it to ssize_t

--- ssize_t/t.c
+++ ssize_t/t.c
@@ -11,34 +11,36 @@ int main()
   ssize_t s = -1;
   int i = (int)s;
 
-  printf(".\n");
+  printf(".0\n");
   printf("%zd %d\n", s, i);
 
   s = INT_MAX;
   i = (int)s;
 
-  printf(".\n");
+  printf(".1\n");
   printf("%zd %d\n", s, i);
   printf("%d %d\n", s, i);
 
   s = INT_MAX + 1;
   i = (int)s;
 
-  printf(".\n");
+  printf(".2\n");
   printf("%zd %d\n", s, i);
-  printf("%d %d\n", s, i);
-
-  //s = INT_MAX + 1;
-  //i = (int)s;
-
-  printf(".\n");
   printf("%lld %d\n", s, i);
 
-  printf(".\n");
+  s = (ssize_t)INT_MAX + 1; // <------------------------ YES !
+  i = (int)s;
+
+  printf(".3\n");
+  printf("%lld %d\n", s, i);
+
+  printf(".4\n");
   printf("ssize_t > int ? %i\n", (s > i));
   printf("ssize_t == int ? %i\n", (s == i));
 
-  printf(".\n");
+  printf(".5\n");
+
+  printf("SSIZE_MAX %lld\n", SSIZE_MAX);
 
   return 0;
 }
